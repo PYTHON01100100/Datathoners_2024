663ab2c293627e0a81b48c736de0411bd33dad50
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+PYTHON01100100@users.noreply.github.com>
2024-10-19 16:50:08 +0300

    Add files via upload

---
+++ Demo/fe/src/layouts/config-nav-workspace.tsx
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------
+
+export const _workspaces = [
+  {
+    id: 'team-1',
+    name: 'Dathoners',
+    logo: `/assets/icons/workspaces/logo-1.webp`,
+    plan: '👋',
+  },
+];
